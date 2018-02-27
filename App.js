56e4479886ab0fa8c25a6695aaf8b5e9077057d2
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-02-27 09:19:27 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,158 @@
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image, 
+  ImageBackground,
+  Linking
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import SafariView from 'react-native-safari-view';
+
+export default class App extends React.Component {
+  state = {
+    user: undefined // User has not signed in yet
+  };
+
+  // Set up OAuth2.0 Linking
+  componentDidMount() {
+    // Adds event listener to handle OAuthLogin:// URLs
+    Linking.addEventListener('url', this.handleOpenURL);
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        this.handleOpenURL({ url });
+      }
+    });
+  };
+
+  componentWillUnmount() {
+    // Remove event listener
+    Linking.removeEventListener('url', this.handleOpenURL)
+  };
+
+  handleOpenURL = ({ url }) => {
+    //Regex expression to extract stringified user string out of the URL
+    const [, user_string] = url.match(/user=([^#]+)/);
+    this.setState({
+      // Parses user string into JSON
+      user: JSON.parse(decodeURI(user_string))
+    });
+
+    if (Platform.OS === 'ios') {
+      SafariView.dismiss();
+    }
+  };
+
+  // Button to handle login with Facebook
+  loginWithFacebook = () => this.openURL('http://alex.local:3000/auth/facebook');
+
+  // Button to handle login with Google
+  loginWithGoogle = () => this.openURL('http://alex.local:3000/auth/google');
+
+
+  // Open URL in a browser
+
+  openURL = (url) => {
+    console.log("It works")
+    if (Platform.OS ==='ios'){
+      SafariView.show({
+        url: url,
+        fromBottom: true,
+      });
+    } else {
+      Linking.openURL(url);
+    }
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <View style={styles.container}>
+        <Image source={require('./home-background.jpg')} height="502" />
+        { user 
+          ?
+            <View style={styles.content}>
+              <Text style={styles.header}>
+                Welcome {user.name}!
+              </Text>
+              <View style={styles.avatar}>
+                <Image source={{ uri: user.avatar}} style={styles.avatarImage} />
+              </View>
+            </View>
+          :
+            <View style={styles.content}>
+              <Text style={styles.header}>
+                Welcome Stranger!
+              </Text>
+              <View style={styles.avatar}>
+                <Icon name="user-circle" size={100} color="rgba(0,0,0,.09)" />
+              </View>
+              <Text style={styles.text}>
+                Please log in to continue {'\n'}
+                to see what's in your city!
+              </Text>
+            </View>
+        }
+        {/*Login Buttons*/}
+        <View style={styles.buttons}>
+          <Icon.Button name="facebook" 
+                       backgroundColor="#3b5998" 
+                       onPress={this.loginWithFacebook} 
+                       {...iconStyles}
+          > Login with Facebook
+          </Icon.Button>
+          <Icon.Button name="google"
+                       backgroundColor="#DD4B39"
+                       onPress={this.loginWithGoogle} 
+                       {...iconStyles}
+          > Login with Google
+          </Icon.Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const iconStyles = {
+  borderRadius: 10,
+  iconStyle: {paddingVertical: 5}
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'azure'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: 20
+  },
+  avatarImage: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  text: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5
+  },
+  buttons: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    margin: 20,
+    marginBottom: 30
+  }
+});
---
+++ backend/server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import passport from 'passport';
+import FacebookStrategy from 'passport-facebook';
+import GoogleStrategy from 'passport-google-oauth20';
+
+import {facebook, google} from './config';
+
+const transformFacebookProfile = (profile) => ({
+    name: profile.name,
+    avatar: profile.picture.data.url
+});
+
+const transformGoogleProfile = (profile) => ({
+    name: profle.displayName,
+    avatar: profile.image.url
+});
+
+passport.use(new FacebookStrategy(facebook,
+    async (accessToken, refreshToken, profile, done) =>
+    done(null, transformFacebookProfile(profile._json))
+));
+
+passport.use(new GoogleStrategy(google, 
+    async (accessToken, refreshToken, profile, done) => 
+    done(null, transformGoogleProfile(profile._json))
+));
+
+// Serialize user into sessions
+passport.serializeUser((user, done) => done(null, user));
+
+// Deserialize user from sessions
+passport.deserializeUser((user, done) => done(null, user));
+
+
+// Initialize http server
+const app = express()
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Set up Facebook auth routes
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', 
+    passport.authenticate('facebook', {failureRedirect: '/auth/facebook'}),
+    (req, res) => res.redirect('OAuthLogin://login?user=' +JSON.stringify(req.user)));
+
+
+// Set up Google auth routes
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile']}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {failureRedirect: '/auth/google'}),
+    (req, res) => res.redirect('OAuthLoging://login?user=' + JSON.stringify(req.user)));
+
+const server = app.listen(3000, () => {
+    const {address, port} = server.address();
+    console.log(`Listening at http://${address}:${port}`)
+});
